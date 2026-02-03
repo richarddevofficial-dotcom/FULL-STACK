6d122565f6819307c288a63972d418994fe27c1f
richarddevofficial-dotcom <richard.dev.official@gmail.com>
2026-02-03 10:16:10 +0200

    updated js

--- week4/class-attendance-analyzer/student.js
+++ week4/class-attendance-analyzer/student.js
@@ -1,23 +1,72 @@
-// FIX: define attendance first
-const attendance = [1, 0, 1, 1, 0, 1, 0, 1, 1, 0];
-// 1 = Present, 0 = Absent
+// // Array of attendances
+// const attendance = [1, 0, 1, 1, 0, 1, 0, 1, 1, 0];
+// // 1 = Present, 0 = Absent
 
-// This function is for Logic & calculations
+// // This function is for Logic & calculations
 
-function analyzeAttendance(attendanceList) {
+// function analyzeAttendance(attendanceList) {
+//   let present = 0;
+//   let absent = 0;
+
+//   // for loop to count attendance
+//   for (let std = 0; std < attendanceList.length; std++) {
+//     if (attendanceList[std] === 1) {
+//       present++;
+//     } else {
+//       absent++;
+//     }
+//   }
+
+//   const totalDays = attendance.length;
+//   const percentage = (present / totalDays) * 100;
+
+//   let status;
+//   if (percentage >= 80) {
+//     status = "EXCELLENT";
+//   } else if (percentage >= 60) {
+//     status = "OK";
+//   } else {
+//     status = "POOR";
+//   }
+
+//   return {
+//     totalDays: totalDays,
+//     present: present,
+//     absent: absent,
+//     percentage: percentage,
+//     status: status,
+//   };
+// }
+
+// // arrow function for Output & presentation
+// const printReport = (report) => {
+//   console.log("Attendance Report");
+//   console.log("Days:", report.totalDays);
+//   console.log("Present:", report.present);
+//   console.log("Absent:", report.absent);
+//   console.log("Percentage:", report.percentage + "%");
+//   console.log("Status:", report.status);
+// };
+
+// // run program
+// const result = analyzeAttendance(attendance);
+// printReport(result);
+
+const attendance = [1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0];
+
+function analyzeAttendance(studentList) {
   let present = 0;
   let absent = 0;
 
-  // for loop to count attendance
-  for (let i = 0; i < attendanceList.length; i++) {
-    if (attendanceList[i] === 1) {
+  for (let std = 0; std < studentList.length; std++) {
+    if (studentList[std] === 1) {
       present++;
-    } else {
+    } else if (studentList[std] === 0) {
       absent++;
     }
   }
 
-  const totalDays = attendanceList.length;
+  const totalDays = attendance.length;
   const percentage = (present / totalDays) * 100;
 
   let status;
@@ -30,16 +79,15 @@ function analyzeAttendance(attendanceList) {
   }
 
   return {
-    totalDays: totalDays,
-    present: present,
-    absent: absent,
-    percentage: percentage,
-    status: status,
+    totalDays,
+    present,
+    absent,
+    percentage: percentage.toFixed(2),
+    status,
   };
 }
 
-// arrow function for Output & presentation
-const printReport = (report) => {
+const reportPrint = (report) => {
   console.log("Attendance Report");
   console.log("Days:", report.totalDays);
   console.log("Present:", report.present);
@@ -48,6 +96,5 @@ const printReport = (report) => {
   console.log("Status:", report.status);
 };
 
-// run program
 const result = analyzeAttendance(attendance);
-printReport(result);
+reportPrint(result);
