d5a2ebaa3741ae66bf543c88bdd006d4f3471fc0
richarddevofficial-dotcom <richard.dev.official@gmail.com>
2026-02-18 09:32:23 +0200

    updated

--- week6/day2/rest.js
+++ week6/day2/rest.js
@@ -27,20 +27,9 @@
 //   }
 // }
 
-// console.log(flexCalc("+", 1, 2, 3, 4));
-// // 10
-
-// console.log(flexCalc("*", 2, 3, 4));
-// // 24
-
-// console.log(flexCalc("-", 10));
-// // 10  (10 - 0)
-
-// console.log(flexCalc("/", 20));
-// // "Error: Division by zero"
+// console.log(flexCalc("/", 1, 2, 0, 4));
 
 const flexCalc = (operator, ...numbers) => {
-  //check 2 number in array
   if (numbers.length < 2) {
     numbers = [...numbers, 0];
   }
@@ -48,21 +37,18 @@ const flexCalc = (operator, ...numbers) => {
   switch (operator) {
     case "+":
       return numbers.reduce((total, num) => total + num, 0);
-
     case "-":
       return numbers.reduce((total, num) => total - num);
-
     case "*":
       return numbers.reduce((total, num) => total * num, 1);
     case "/":
-      //validation to start from index 1 and check 0
       if (numbers.slice(1).includes(0)) {
-        return "error can not divide by zero";
+        return "error not divisor by zero";
       }
       return numbers.reduce((total, num) => total / num);
     default:
-      return "error";
+      return "invalid";
   }
 };
 
-console.log(flexCalc("-", 4, 0, 6));
+console.log(flexCalc("+", 1, 2, 3));
